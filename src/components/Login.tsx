38f6ac0fce1ec3e1b3f6ee77bdae29261f70fb1e
faisalhardin <rangkuti.hardin@gmail.com>
2024-11-03 08:27:03 +0700

    feat(wmd): store jwt token

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,14 +5,22 @@ const  LoginPage = () => {
 
     const googleLogin = useGoogleLogin({
       onSuccess: async (codeResponse) => {
-        const tokenResponse = await axios.get(
-          `http://127.0.0.1:8080/v1/auth/google/callback?code=${codeResponse.code}`
-        );
-        console.log("tokenResponse",tokenResponse);
-        sessionStorage.setItem("jwt_token", tokenResponse.data.token);
-        sessionStorage.setItem("jwt_token", tokenResponse.data.token);
 
+        try {
+          const tokenResponse = await axios.get(
+            `http://127.0.0.1:8080/v1/auth/google/callback?code=${codeResponse.code}`
+          );
+          if (tokenResponse.data.token) {
+            sessionStorage.setItem("jwt_token", tokenResponse.data.token);
+            console.log("JWT token stored successfully.");
+          } else {
+            console.error("Token not received in response.");
+          }
+        } catch (error) {
+          console.error("Error during token retrieval:", error);
+        }
       },
+      onError: (error) => console.error("Login failed:", error),
       flow: "auth-code",
     })
     return (
