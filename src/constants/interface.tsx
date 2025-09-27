bc01cf9ebe059e5c2b2d9d77d9d470b598caf99d
faisalhardin <rangkuti.hardin@gmail.com>
2025-09-27 22:23:46 +0700

    feat(wmd): redirect invalid refresh token

--- src/constants/interface.tsx
+++ src/constants/interface.tsx
@@ -1,8 +1,8 @@
 
 interface JwtClaims {
-    sub: string;
-    exp?: number;
-    iat?: number;
+    sub?: string;
+    exp: number;
+    iat: number;
     aud?: string;
     iss?: string;
     [key: string]: any;
