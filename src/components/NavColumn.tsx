bebe1b7bfb8c4d99f860d6054c7f82ab9a9cd865
faisalhardin <rangkuti.hardin@gmail.com>
2025-11-19 10:35:10 +0700

    feat(wmd): mobile portability

--- src/components/NavColumn.tsx
+++ src/components/NavColumn.tsx
@@ -155,7 +155,9 @@ function ColumnNav({ isMobileNavOpen = false, setIsMobileNavOpen }: ColumnNavPro
             <div className="w-6 h-6 border-2 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
           </div>
         ) : isAuthenticated ? (
-          <UserComponent />
+          <div className="w-full">
+            <UserComponent/>
+          </div>
         ) : (
           <Login />
         )}
